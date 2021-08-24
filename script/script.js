c2c5d548253048ad836961c6295d6f1bd590b4ca
Andru <rodomir3119841985@gmail.com>
2021-08-24 09:13:09 +0300

    lessons4

--- script/script.js
+++ script/script.js
@@ -30,27 +30,93 @@
         }
     }
 // кнопка вверх
-    
-function trackScrill() {
-    let scrolled = window.pageXOffset;
-    let cords = document.documentElement.clientHeight;
- 
-    if(scrolled>cords) {
-        goTopBtn.classList.add('topButton__show');
-    } else {
-        goTopBtn.classList.remove('topButton__show');
+    {
+        function trackScrill() {
+            let scrolled = window.pageXOffset;
+            let cords = document.documentElement.clientHeight;
+         
+            if(scrolled>cords) {
+                goTopBtn.classList.add('topButton__show');
+            } else {
+                goTopBtn.classList.remove('topButton__show');
+            }
+        
+        }
+        function backToTop(){
+            if(window.pageXOffset > 0) {
+                window.scrollBy(0,-80);
+                setTimeout(backToTop,0)
+            }
+        }
+        const goTopBtn = document.querySelector('.topButton');
+        window.addEventListener('scroll',trackScrill);
+        goTopBtn.addEventListener('click',backToTop);
+        
+        
     }
-
-}
-function backToTop(){
-    if(window.pageXOffset > 0) {
-        window.scrollBy(0,-80);
-        setTimeout(backToTop,0)
+    // slider
+    {
+        {
+            class SlideShow {
+              constructor(startIndex, element) {
+                this.startIndex = startIndex;
+                this.currentIndex = this.startIndex;
+                this.element = element;
+                this.slides = this.element.querySelectorAll('.slideshow__slider');
+                
+                this.setActiveSlide();
+                this.next();
+                this.prev();
+              }
+        
+              setActiveSlide() {
+                this.slides.forEach((item, index) => {
+                  if (index === this.currentIndex
+                  ) {
+                    item.classList.add('active');
+                  }
+                  else {
+                    item.classList.remove('active');
+                  }
+                })
+              }
+        
+              next() {
+                let nextBtn = this.element.querySelector('[data-way="next"]');
+                nextBtn.addEventListener('click', () => {
+                  if (this.currentIndex === this.slides.length - 1
+                  ) {
+                    this.currentIndex = 0;
+                  }
+                  else {
+                    this.currentIndex++;
+                  }
+                  this.setActiveSlide();
+                })
+              }
+        
+              prev() {
+                let prevBtn = this.element.querySelector('[data-way="prev"]');
+                prevBtn.addEventListener('click', () => {
+                  if (this.currentIndex === 0
+                  ) {
+                    this.currentIndex = this.slides.length - 1;
+                  }
+                  else {
+                    this.currentIndex--;
+                  }
+                  this.setActiveSlide();
+                })
+              }
+            }
+        
+            let slideShow = document.querySelectorAll('.slideshow');
+            slideShow.forEach(item => {
+                new SlideShow(0, item)
+              })
+          };
+       
     }
-}
-const goTopBtn = document.querySelector('.topButton');
-window.addEventListener('scroll',trackScrill);
-goTopBtn.addEventListener('click',backToTop);
 
 
     
